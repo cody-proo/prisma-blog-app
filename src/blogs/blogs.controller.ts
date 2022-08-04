11d15362d9b3f4c941084d37d1d4808d91d07b36
cody-proo <cody18v@gmail.com>
2022-08-04 23:46:43 +0430

    add oop to blogs.controller

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -3,91 +3,90 @@ import { Response } from "express";
 import { ExpressRequest } from "../request";
 import { ICreateBlogBody, IUpdateBlogBody } from "./blogs.type";
 
-const prismaClient = new PrismaClient();
-
-export const createBlog = async (
-  request: ExpressRequest<ICreateBlogBody>,
-  response: Response
-) => {
-  const { title, description } = request.body;
-  const titleTakenBefore = await prismaClient.blog.findFirst({
-    where: { title },
-    select: {
-      id: true,
-    },
-  });
-  if (titleTakenBefore) {
-    return response.status(400).json({ message: "BadRequestException" });
+export class BlogsController {
+  private client: PrismaClient;
+  constructor() {
+    this.client = new PrismaClient();
   }
-  const blog = await prismaClient.blog.create({
-    data: {
-      title,
-      description,
-      creatorId: request.user.id,
-    },
-    include: {
-      creator: {},
-    },
-  });
-  return response.status(201).json(blog);
-};
 
-export const getBlogs = async (request: ExpressRequest, response: Response) => {
-  const blogs = await prismaClient.blog.findMany({
-    include: { creator: true, comments: true },
-  });
-  return response.json({ blogs });
-};
+  createBlog = async (
+    request: ExpressRequest<ICreateBlogBody>,
+    response: Response
+  ) => {
+    const { title, description } = request.body;
+    const titleTakenBefore = await this.client.blog.findFirst({
+      where: { title },
+      select: {
+        id: true,
+      },
+    });
+    if (titleTakenBefore) {
+      return response.status(400).json({ message: "BadRequestException" });
+    }
+    const blog = await this.client.blog.create({
+      data: {
+        title,
+        description,
+        creatorId: request.user.id,
+      },
+      include: {
+        creator: {},
+      },
+    });
+    return response.status(201).json(blog);
+  };
 
-export const getSingleBlog = async (
-  request: ExpressRequest,
-  response: Response
-) => {
-  const blogId: string = request.params.id;
-  const blog = await prismaClient.blog.findUnique({
-    where: { id: blogId },
-    include: { creator: true, comments: true },
-  });
-  if (!blog) {
-    return response.status(404).json({ message: "Not Found" });
-  }
-  return response.json({ blog });
-};
+  getBlogs = async (request: ExpressRequest, response: Response) => {
+    const blogs = await this.client.blog.findMany({
+      include: { creator: true, comments: true },
+    });
+    return response.json({ blogs });
+  };
 
-export const deleteBlog = async (
-  request: ExpressRequest,
-  response: Response
-) => {
-  const blogId = request.params.id;
-  const blog = await prismaClient.blog.findFirst({
-    where: { id: blogId, creatorId: request.user.id },
-  });
-  if (!blog) {
-    return response.status(404).json({ message: "Not Found" });
-  }
-  const deletedDocument = await prismaClient.blog.delete({
-    where: { id: blogId },
-  });
-  if (!deletedDocument) {
-    return response.status(404).json({ message: "Not Found" });
-  }
-  return response.status(200).json({ message: "delete successfully" });
-};
+  getSingleBlog = async (request: ExpressRequest, response: Response) => {
+    const blogId: string = request.params.id;
+    const blog = await this.client.blog.findUnique({
+      where: { id: blogId },
+      include: { creator: true, comments: true },
+    });
+    if (!blog) {
+      return response.status(404).json({ message: "Not Found" });
+    }
+    return response.json({ blog });
+  };
 
-export const updateBlog = async (
-  request: ExpressRequest<IUpdateBlogBody>,
-  response: Response
-) => {
-  const blogId = request.params.id;
-  const updatedDocument = await prismaClient.blog.update({
-    where: { id: blogId },
-    data: request.body,
-    include: {
-      creator: true,
-    },
-  });
-  if (!updatedDocument) {
-    return response.status(404).json({ message: "Not Found" });
-  }
-  return response.status(200).json({ blog: updatedDocument });
-};
+  deleteBlog = async (request: ExpressRequest, response: Response) => {
+    const blogId = request.params.id;
+    const blog = await this.client.blog.findFirst({
+      where: { id: blogId, creatorId: request.user.id },
+    });
+    if (!blog) {
+      return response.status(404).json({ message: "Not Found" });
+    }
+    const deletedDocument = await this.client.blog.delete({
+      where: { id: blogId },
+    });
+    if (!deletedDocument) {
+      return response.status(404).json({ message: "Not Found" });
+    }
+    return response.status(200).json({ message: "delete successfully" });
+  };
+
+  updateBlog = async (
+    request: ExpressRequest<IUpdateBlogBody>,
+    response: Response
+  ) => {
+    const blogId = request.params.id;
+    const updatedDocument = await this.client.blog.update({
+      where: { id: blogId },
+      data: request.body,
+      include: {
+        creator: true,
+      },
+    });
+    if (!updatedDocument) {
+      return response.status(404).json({ message: "Not Found" });
+    }
+    return response.status(200).json({ blog: updatedDocument });
+  };
+}
